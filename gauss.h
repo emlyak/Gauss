2be96a04ad280c67751195c9f655065d3bb449c6
Egor Lyakhovets <em@lyakhovets.com>
2024-09-17 17:52:31 +0700

    v1.0

--- gauss.h
+++ gauss.h
@@ -1,9 +1,20 @@
 #ifndef GAUSS_H
 #define GAUSS_H
 
+#include <vector>
+#include <iterator>
+
+using vec_d = std::vector<double>;
+using matrix = std::vector<vec_d>;
+
 class Gauss
 {
-    
-}
+public:
+    static
+    vec_d solve(
+        matrix a,
+        vec_d b
+    );
+};
 
 #endif
